13383ab79237c3365267e4c6aa4039a2d0217200
olgamperez <ing.olgaperez@gmail.com>
2019-08-28 18:33:34 -0400

    taller de promesas  funcion readFile con promesa

--- index.js
+++ index.js
@@ -2,27 +2,37 @@
 //   // ...
 // };
 const fs= require('fs');
-
 const marked= require('marked');
+
 // fs.readFile(./)
-const links= (path)=>{
-  fs.readFile(path, 'utf8', (err, data)=>{
-  if(err){
-throw err
-  }
-
-let links= []//es para guardar los documentos
-const renderer = new  marked.Renderer();//new es para crear un nueva instancia
-renderer.link= function(href, title, text){
-links.push({
-  href:href,
-  text:text,
-  file:path
-});
+const readFile = (path)=>{
+  return new Promise ((resolve, reject) =>{
+    let links= [];
+    fs.readFile(path, 'utf8', (err, data)=>{
+      if(err){
+    throw err
+      }
+      else{
+        const renderer = new  marked.Renderer();//new es para crear un nueva instancia
+        renderer.link= function(href, title, text){
+          links.push({
+            href:href,
+            text:text,
+            file:path
+          });
+        };
+        marked(data, {renderer:renderer})
+        const renderer = new  marked.Renderer();//new es para crear un nueva instancia
+        renderer.link= function(href, title, text){
+          links.push({
+            href:href,
+            text:text,
+            file:path
+          });
+        };
+        marked(data, {renderer:renderer})
+        resolve(links);
+      };
+    })
+  })
 }
-marked(data, {renderer:renderer})
-console.log(links);
-})
-}
-
-console.log(links('prueba.md'));
---
+++ taller promesas/indexPromise.js
@@ -0,0 +1,33 @@
+
+//promesa: es lo mismo que la vida real... si yo les primeto algo por ejemplo que el gato este vivo todas estaran fecies y si el gato muere todas estaran triste
+
+//estan en tres estados:
+//completa- rechazada- en espera
+
+// let myFirstPromise = promise.resolve('hola mundo')
+
+// myFirstPromise
+// .then(response =>{
+//     console.log(response)
+// });
+
+// let mySecondPromise = new promise(resolve, rejects) =>{
+//     setTimeout( ()=>{
+            resolve
+//     })
+// }
+
+//fetch
+const fetch = require('fetch');
+const fetchUrl = fetch.fetchUrl(res);
+
+
+//fetchUrl de un sitio............. 200 es cuando esta activo un sitio
+const getdata = (url) =>{
+    return new Promise((resolve, reject) =>{
+        if(meta)
+    })
+}
+
+
+
